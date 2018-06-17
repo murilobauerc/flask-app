e9ed29b2701d82a5ba56536a853b3286788a6019
murilobauerc <panchobauer@gmail.com>
2018-06-17 18:06:33 -0300

    refatoração no código app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+''' importamos a classe base do Flask, ou seja, o Werkzeug que é uma biblioteca WSGI para enfim ter a abstração de métodos simples
+    e termos o workflow que desejamos.
+'''
 from flask import Flask
 
-app = Flask(__name__)
+# criamos uma instancia da classe base do Flask que chamaremos de app
+app = Flask('flask-app')
 
 @app.route("/")
 def hello_world():
